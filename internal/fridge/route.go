9b5591fbc9f6fe592b8d5dcaa39081ef448f7076
bingodfok <bingodfok@gmail.com>
2025-05-31 19:53:27 +0800

    食物分类列表

--- internal/fridge/route.go
+++ internal/fridge/route.go
@@ -11,6 +11,6 @@ func Route(ctx *ctx.AppContext) func(router fiber.Router) {
 		router.Post("/fridge/create_fridge", handler.CreateFridgeHandler(ctx))
 		router.Get("/fridge/list", handler.FridgeListHandler(ctx))
 		router.Post("/fridge/edit", handler.FridgeEditHandler(ctx))
-		router.Put("/category/list", handler.CategoryListHandler(ctx))
+		router.Get("/category/list", handler.CategoryListHandler(ctx))
 	}
 }
